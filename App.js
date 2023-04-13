ad160ead36acc16453a19decaca24f9d2e6a26a2
Drew Code <pekke100@gmail.com>
2023-04-13 16:30:27 -0700

    added chat component

--- App.js
+++ App.js
@@ -3,19 +3,47 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-web';
 
 export default function App() {
+  const [inputMessage,setInputMessage] = usteState("")
+  const [outputMessage,setOutputMessage] = usteState("results to be shown here")
+    const handleButtonClick=()=>{
+    console.log(inputMessage)
+    fetch("https://api.openai.com/v1/chat/completion", {
+      method:"POST",
+      headers:{
+          "Content-Type": "application/json",
+          "Authoriztaion": "Bearer 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"
+      },
+      body:JSON.stringify({
+        "messages": [{"role": "user", "content": inputMessage}] ,
+        "model": "gpt-3.5-turbo",
+      })
+    }).then((response)=>response.json).then((data)=>{
+      console.logo(data.choices[0].message.content)
+      setOutputMessage(data.choices[0].message.content.trim())
+    })
+  }
+
+
+  const handleTextInput=(text)=>{
+    swtInputMessage(text)
+    console.log(text)
+  }
+ 
+//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
+  
   return (
     <View style={styles.container}>
       <view style={{ flex:1, justifyContent: "center" }}>
-      <Text>Results Will Be showed here!</Text>
+      <Text>{outputMessage}</Text>
       </view>
       
       <view style={{ flexDirection: "row" }}>
         <view style={{ flex:1, marginLeft: 10 }}>
-        <TextInput placeholder= 'Enter your question '></TextInput>
+        <TextInput placeholder= 'Enter your question ' onChangeText={handleTextInput}></TextInput>
         </view>
       
       
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleButtonClick}>
         <view> style={{ backgroundColor: "red", padding: 5, marignRight: 10, marginBottom:20 }}
         <Text>Sends</Text>
         </view>
