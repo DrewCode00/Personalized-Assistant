c1b369927398f5e37ddaa7ab55bc5571378379d6
Drew Code <pekke100@gmail.com>
2023-04-13 15:38:38 -0700

    -view commponent is implemented

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <view style={{ flex:1, justifyContent: "center" }}>
+      <Text>Results Will Be showed here!</Text>
+      </view>
+      
+      <view style={{ flexDirection: "row" }}>
+        <view style={{ flex:1, marginLeft: 10 }}>
+        <TextInput placeholder= 'Enter your question '></TextInput>
+        </view>
+      
+      
+      <TouchableOpacity>
+        <view> style={{ backgroundColor: "red", padding: 5, marignRight: 10, marginBottom:20 }}
+        <Text>Sends</Text>
+        </view>
+      
+      </TouchableOpacity>
+      </view>
+      
+      
       <StatusBar style="auto" />
     </View>
   );
